83bec7cb7dbc693e09cdda809a7cd4efbc4bb122
ThuyPhan <thuykara21@gmail.com>
2023-03-09 14:56:49 +0700

    commit Build framework: Multiple browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,30 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest{
+	private WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+	
+	protected WebDriver getBrowserDriver(String browserName) {	
+		switch (browserName) {
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		default:
+			throw new RuntimeException("Please input with correct browser name. ");	
+		}	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return driver;
+	}
 }
